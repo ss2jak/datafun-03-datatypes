c26fe1bb7ac9a9a95bbfad92790b1794cf8fd0e6
Your Name <you@example.com>
2023-02-01 00:01:45 -0600

    Project3

---
+++ user_string_lists_out.py
@@ -0,0 +1,42 @@
+Team_outcome:  [('Eagles', 'win'), ('Bengals', 'lose'), ('Chiefs', 'draw')]
+
+Player-Offense:  {('Mahomes', 'threw', 'it'), ('Burrow', 'ran', 'pig skin'), ('Hurts', 'caught', 'football')}
+
+List lengths:  [4, 3, 4, 3]
+Bengals
+
+threw
+
+Hurts
+
+win
+
+football
+
+
+
+
+Eagles had a win in their game.
+Bengals had a lose in their game.
+Chiefs had a draw in their game.
+Hurts almost ran the ball.
+Woodchuck.txt
+
+How much wood would a woodchuck chuck
+if a woodchuck could chuck wood?
+He would chuck, he would, as much as he could,
+and chuck as much wood as a woodchuck would
+if a woodchuck could chuck wood.
+
+
+List of Woodchuck
+['How', 'much', 'wood', 'would', 'a', 'woodchuck', 'chuck', 'if', 'a', 'woodchuck', 'could', 'chuck', 'wood?', 'He', 'would', 'chuck,', 'he', 'would,', 'as', 'much', 'as', 'he', 'could,', 'and', 'chuck', 'as', 'much', 'wood', 'as', 'a', 'woodchuck', 'would', 'if', 'a', 'woodchuck', 'could', 'chuck', 'wood.']
+
+Unique words in text
+{'as', 'could,', 'he', 'would,', 'chuck', 'wood?', 'and', 'would', 'wood.', 'wood', 'much', 'He', 'chuck,', 'could', 'How', 'a', 'if', 'woodchuck'}
+
+['He', 'How', 'a', 'a', 'a', 'a', 'and', 'as', 'as', 'as', 'as', 'chuck', 'chuck', 'chuck', 'chuck', 'chuck,', 'could', 'could', 'could,', 'he', 'he', 'if', 'if', 'much', 'much', 'much', 'wood', 'wood', 'wood.', 'wood?', 'woodchuck', 'woodchuck', 'woodchuck', 'woodchuck', 'would', 'would', 'would', 'would,']
+['would,', 'would', 'would', 'would', 'woodchuck', 'woodchuck', 'woodchuck', 'woodchuck', 'wood?', 'wood.', 'wood', 'wood', 'much', 'much', 'much', 'if', 'if', 'he', 'he', 'could,', 'could', 'could', 'chuck,', 'chuck', 'chuck', 'chuck', 'chuck', 'as', 'as', 'as', 'as', 'and', 'a', 'a', 'a', 'a', 'How', 'He']
+
+How many words are in the text?
+195
